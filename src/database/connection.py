96c791c23b38df19f2ff0442477c941778ed215a
Becky-Kwon <beckykwon0623@gmail.com>
2023-12-10 01:12:32 +0900

    Todo Class 만듬

--- src/database/connection.py
+++ src/database/connection.py
@@ -2,3 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "mysql+pymysql://root:todos@127.0.0.1:3306/todos"
+
+engine = create_engine(DATABASE_URL, echo=True)  #사용되는 시점의 sql문 print 되도록 echo= True
+SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+session = SessionFactory()
+
+
+#session.query() #DB와 통신
